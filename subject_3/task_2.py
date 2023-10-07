5e2e0def2d3f369161d5091728476ab199a79891
kxhcfk <reveersed@mail.ru>
2023-10-07 17:19:45 +0300

    feat: 3 subject, task 2

---
+++ subject_3/task_2.py
@@ -0,0 +1,33 @@
+# Дано слово из маленьких латинских букв.
+# Сколько там согласных и гласных букв?
+# Гласными называют буквы «a», «e», «i», «o», «u».
+# Для решения задачи создайте переменную и в неё положите слово спомощью input()
+# А также определите количество каждой из этих гласных букв
+# Если какой-то из перечисленных букв нет - Выведите False
+
+word = input("Введите слово: ")
+
+vowel_letters = 'aeiou'
+
+a = word.count('a')
+e = word.count('e')
+i = word.count('i')
+o = word.count('o')
+u = word.count('u')
+y = word.count('y')
+
+if a == 0:
+    print("a = False")
+if e == 0:
+    print("e = False")
+if i == 0:
+    print("i = False")
+if o == 0:
+    print("o = False")
+if u == 0:
+    print("u = False")
+if y == 0:
+    print("y = False")
+
+print(f"Гласных: {a + e + i + o + u}")
+print(f"Согласных: {len(word) - (a + e + i + o + u)}")
